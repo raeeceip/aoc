d82e2453551cec4b5dde540194ea2ae557740063
raeeceip <chiboguchisomu@gmail.com>
2024-12-01 01:17:54 -0500

    Add initial code for 2024 Advent of Code challenge

---
+++ 2024/library/files.go
@@ -0,0 +1,52 @@
+package aoc
+
+import (
+	"bufio"
+	"log"
+	"os"
+)
+
+func ReadFileLineByLine(path string) []string {
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	var output []string
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		output = append(output, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return output
+}
+
+func ReadFileAsString(path string) string {
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Scan()
+	return scanner.Text()
+}
+
+func ReadFileAsInt(path string) int {
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Scan()
+	return FetchNumFromStringIgnoringNonNumeric(scanner.Text())
+}
---
+++ 2024/library/strings.go
@@ -0,0 +1,80 @@
+package aoc
+
+import (
+	"strconv"
+	"strings"
+	"unicode"
+)
+
+func FetchSliceOfIntsInString(line string) []int {
+	nums := []int{}
+	var build strings.Builder
+	isNegative := false
+	for _, char := range line {
+		if unicode.IsDigit(char) {
+			build.WriteRune(char)
+		}
+
+		if char == '-' {
+			isNegative = true
+		}
+
+		if (char == ' ' || char == ',' || char == '~') && build.Len() != 0 {
+			localNum, err := strconv.ParseInt(build.String(), 10, 64)
+			if err != nil {
+				panic(err)
+			}
+			if isNegative {
+				localNum *= -1
+			}
+			nums = append(nums, int(localNum))
+			build.Reset()
+			isNegative = false
+		}
+	}
+	if build.Len() != 0 {
+		localNum, err := strconv.ParseInt(build.String(), 10, 64)
+		if err != nil {
+			panic(err)
+		}
+		if isNegative {
+			localNum *= -1
+		}
+		nums = append(nums, int(localNum))
+		build.Reset()
+	}
+	return nums
+}
+
+func FetchNumFromStringIgnoringNonNumeric(line string) int {
+	var build strings.Builder
+	for _, char := range line {
+		if unicode.IsDigit(char) {
+			build.WriteRune(char)
+		}
+	}
+	if build.Len() != 0 {
+		localNum, err := strconv.ParseInt(build.String(), 10, 64)
+		if err != nil {
+			panic(err)
+		}
+		return int(localNum)
+	}
+	return 0
+}
+
+func SplitStringAfter(input string, length int) (output []string) {
+	startIndex := 0
+	for startIndex < len(input) {
+		output = append(output, input[startIndex:startIndex+length])
+		startIndex += length
+	}
+	return
+}
+
+func Get2DGrid(input []string) (grid [][]string) {
+	for _, line := range input {
+		grid = append(grid, strings.Split(line, ""))
+	}
+	return
+}
